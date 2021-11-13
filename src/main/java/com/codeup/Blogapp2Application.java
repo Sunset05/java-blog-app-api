6b7bcc5ca94eb8ae8389bd3bcf5c3148e69e2712
Keith Funk <keith.c.funk@gmail.com>
2021-11-13 11:02:53 -0700

    starer boilerplate for spring web API REST

---
+++ src/main/java/com/codeup/Blogapp2Application.java
@@ -0,0 +1,13 @@
+package com.codeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogapp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blogapp2Application.class, args);
+    }
+
+}
